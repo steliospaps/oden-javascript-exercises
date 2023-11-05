ecd1361c5f4e97a135778b656e6d86eccaafc305
stelios <stelios05@gmail.com>
2023-11-05 12:51:40 +0000

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.sort((a,b)=>((a.yearOfDeath?a.yearOfDeath:new Date().getFullYear())-a.yearOfBirth)-((b.yearOfDeath?b.yearOfDeath:new Date().getFullYear())-b.yearOfBirth)).pop()
 };
 
 // Do not edit below this line
